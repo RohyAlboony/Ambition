c0ae1417bc50347d9664bb1891668e1aa33d1ca7
RohyAlboony <rohyalhomse@gmail.com>
2021-08-02 16:03:53 +0300

    first commit

---
+++ lib/modules/archived_tasks/archived_tasks_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ArchivedTasksPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Archived Tasks"
+      ),
+    );
+  }
+}
---
+++ lib/modules/done_tasks/done_tasks_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class DoneTasksPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Done Tasks"
+      ),
+    );
+  }
+}
